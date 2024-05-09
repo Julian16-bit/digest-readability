d80436d50b694ddfd4a6ff8f5d8a6071c5ed78ef
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-05-09 09:50:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit as st
 import numpy as np
 import plotly.figure_factory as ff
 import plotly.express as px
+
+df = pd.read_csv('digest_descriptives_merged_simple')
+
+                 
