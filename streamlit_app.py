1556820a9d2b9c5f89d8ff5b22bc11d6ede131e6
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-05-09 10:22:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from scipy.stats import zscore
 
 df = pd.read_csv('digest_descriptives_merged_simple.csv')
 
-tab1, tab2 = st.tabs(["Tab 1)", "Tab 2"])
+tab1, tab2 = st.tabs(["Tab 1", "Tab 2"])
 
 with tab1:
   fig = px.box(df, x='section_chapter', y='gunning_fog')
