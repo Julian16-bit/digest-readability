cb2f07d7fae673605cd144a3c0a0f4c9434c1878
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-05-09 13:57:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,51 @@ with tab1:
       line=dict(color="red", width=2)
     )
     st.plotly_chart(fig, theme=None, use_container_width=True)
+
+with tab2:
+  col1, col2 = st.columns(2)
+  with col1:
+    df_filtered = remove_outliers(df)
+    fig = px.box(df_filtered, x='section_chapter', y='flesch_kincaid_grade')
+    fig.update_layout(xaxis_title='')
+    fig.add_shape(
+      type="line",
+      x0=-1,
+      x1=len(df['section_chapter'].unique()),
+      y0=9, 
+      y1=9, 
+      line=dict(color="red", width=2)
+    )
+    fig.add_shape(
+      type="line",
+      x0=-1,
+      x1=len(df['section_chapter'].unique()),
+      y0=12, 
+      y1=12, 
+      line=dict(color="red", width=2)
+    )
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+  with col2:
+    df1_filtered = remove_outliers(df1)
+    fig = px.box(df1_filtered, x='section_chapter', y='flesch_kincaid_grade')
+    fig.update_layout(xaxis_title='')
+    fig.add_shape(
+      type="line",
+      x0=-1,
+      x1=len(df['section_chapter'].unique()),
+      y0=9, 
+      y1=9, 
+      line=dict(color="red", width=2)
+    )
+    fig.add_shape(
+      type="line",
+      x0=-1,
+      x1=len(df['section_chapter'].unique()),
+      y0=12, 
+      y1=12, 
+      line=dict(color="red", width=2)
+    )
+    st.plotly_chart(fig, theme=None, use_container_width=True)
     
     
   
