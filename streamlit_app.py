b7b2a7f0e64eb75f27e0e54ba5b0cfd92690eede
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-05-09 11:07:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with tab1:
   fig.add_shape(
     type="line",
     x0=0,
-    x1=len(df['section_chapter']),
+    x1=len(df['section_chapter'].unique()),
     y0=9, 
     y1=9, 
     line=dict(color="red", width=2)
