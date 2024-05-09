7e2beac1615f810ec477fe842fa461a5abf4b71b
Heng Zhang <54906687+HengZ121@users.noreply.github.com>
2024-05-09 16:15:21 +0000

    changed tab width and improved dot label with hovers

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import pandas as pd
 from scipy.stats import zscore
 import matplotlib.pyplot as plt
 
+st.set_page_config(layout="wide")
+
 df = pd.read_csv('digest_descriptives_merged_simple.csv')
 
 numeric_columns = df.select_dtypes(include=['number'])
@@ -17,8 +19,17 @@ df_filtered = df[~outlier_indices]
 tab1, tab2 = st.tabs(["Tab 1", "Tab 2"])
 
 with tab1:
-  fig = px.box(df_filtered, x='section_chapter', y='gunning_fog')
-  fig.update_layout(xaxis_title='')
+  fig = px.box(
+    df_filtered, 
+    x='section_chapter', 
+    y='gunning_fog',
+  )
+  fig.update_traces(
+    hovertemplate="%{x} <br>Gunning Fog Index: %{y}"
+  )
+  fig.update_layout(
+    xaxis_title=''
+  )
   st.plotly_chart(fig, theme=None, use_container_width=True)
   
   
